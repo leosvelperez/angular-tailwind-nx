fa0ae23bf47aa92faece87f62bdb10aaef9ef1d6
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-01-24 17:52:34 +0000

    extract the header into a library

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { Lib1Module } from '@angular-tailwind-nx/lib1';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, Lib1Module],
   providers: [],
   bootstrap: [AppComponent],
 })
