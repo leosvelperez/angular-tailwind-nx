93d73d24c5d097951f56be3a86dbde2c1b8f47d3
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-01-26 15:49:36 +0000

    distribute the publishable library styles

--- libs/lib3/src/lib/button/button.component.ts
+++ libs/lib3/src/lib/button/button.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'angular-tailwind-nx-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ButtonComponent {
   @Input() href?: string;
