2ebb9d8f4c7514a798c7342d46815f82c04ee347
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-03-11 11:15:27 +0000

    fix lib1 linting errors

--- libs/lib1/src/lib/header/header.component.ts
+++ libs/lib1/src/lib/header/header.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'angular-tailwind-nx-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
